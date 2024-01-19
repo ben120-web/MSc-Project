42d994eb301d05eeabbdbbafff9b6c183fdb3f6c
B-SECUR\Ben.Russell <ben.russell@b-secur.com>
2024-01-19 12:19:26 +0000

    Adding latin hypercube sampling

--- Electrode Motion Denoising/models/datastore/createSyntheticCleanEcgSignals.m
+++ Electrode Motion Denoising/models/datastore/createSyntheticCleanEcgSignals.m
@@ -21,66 +21,107 @@ featureStruct.zPosition(:, 2) = MAX_Z_POSITION_OF_EXTRAMA';
 featureStruct.width(:, 1) = MIN_GAUSSIAN_WIDTH';
 featureStruct.width(:, 2) = MAX_GAUSSIAN_WIDTH';
 
+% Number of different sigals to generate.
+numOfCleanSignals = 10000;
+
+% Employ Latin Hyper Cube sampling to generate all possible combinations of
+% parameter values.
+[anglesOfExtrema, ~] = latinHyperCubeSampling(numOfCleanSignals, MIN_ANGLES_OF_EXTREMA, MAX_ANGLES_OF_EXTRAMA);
+[zPositionOfExtrema, ~] = latinHyperCubeSampling(numOfCleanSignals, MIN_Z_POSITION_OF_EXTRAMA, MAX_Z_POSITION_OF_EXTRAMA);
+[gaussianWidth, ~] = latinHyperCubeSampling(numOfCleanSignals, MIN_GAUSSIAN_WIDTH, MAX_GAUSSIAN_WIDTH);
+
+% Number of heartrates
+numOfHeartRates = numel(HR_TO_GENERATE);
+
+% Define output folder
+outputFolder = erase(mfilename('fullpath'), mfilename);
+outputFolder = fullfile(outputFolder, ...
+    'cleanSignals');
+
+% Make directory
+if ~isfolder(outputFolder) mkdir(outputFolder); end
 
 % Loop through each parameter setting.
 for iHeartRate = 1 : numOfHeartRates
 
     meanHr = HR_TO_GENERATE(iHeartRate);
 
-    % State number of features we want to iterate through.
-    nFeatures = numel(fieldnames(featureStruct));
+    % Loop through each signal to generate
+    for iSignal = 1 : numOfCleanSignals
 
-    % Few more loops to cover each shape.
-    for iFeature = 1 : nFeatures
+        % Extract data.
+        angleData = anglesOfExtrema(iSignal, :);
+        zData = zPositionOfExtrema(iSignal, :);
+        widthData = gaussianWidth(iSignal, :);
 
-        featureNames = fieldnames(featureStruct);
+        % Call ECGSYN MATLAB function to generate signal.
+        [cleanEcgSignal, ~] = ecgsyn(samplingFrequency, 256, 0, meanHr, ...
+            1, 0.5, samplingFrequency, angleData, zData, ...
+            widthData);
 
-        % Specify feature
-        feature = string(featureNames(iFeature));
+        % Save the outputs with appropriate file names.
+        fileName = fullfile(string(meanHr) + "BPM" + "_cleanSignal");
+        save(fileName, "cleanEcgSignal")
 
-        % Grab Data
-        data = featureStruct.(feature);
-
-        minValues = data(:, 1);
-        maxValues = data(:, 2);
-
-        % Get number of morhologies to iterate through
-        numOfMorphologies = height(data);
-
-        % Iterate through specific feature
-        for iMorphology = 1 : numOfMorphologies
-
-            % Define Morphology to change
-            iterationData = data(iMorphology, :);
-
-            % Define morp type
-            morphType = FEATURES(iMorphology);
-
-            % Logic to determine step size based on feature
-            switch feature
-                case "extremaAngles"
-                    stepSize = 1; % Degrees
-                case "zPosition"
-                    stepSize = 0.1;
-                case "width"
-                    stepSize = 0.05;
-            end
-
-            % Determine numner of iterations
-            nIterations = iterationData(:, 2) - iterationData(:, 1);
-
-            % Determine parameters
-            anglesOfExtrema = []
-            zPositionExtrema = 
-            GaussianWidth = 
-
-            % Call ECGSYN MATLAB function to generate signal.
-            [ecgSignal, peakLocations] = ecgsyn(samplingFrequency, 256, 0, meanHr, ...
-                1, 0.5, samplingFrequency, anglesOfExtrema, zPositionExtrema, ...
-                    GaussianWidth);
-
-            % Save the outputs with appropriate file names.
-            fileName = fullfile(string(meanHr) + "BPM" + "cleanSignal");
-        end
     end
+end
+end
+
+function [X_scaled,X_normalized] = latinHyperCubeSampling(n, min_ranges_p, max_ranges_p)
+%lhsdesign_modified is a modification of the Matlab Statistics function lhsdesign.
+%It might be a good idea to jump straight to the example to see what does
+%this function do.
+%The following is the description of lhsdesign from Mathworks documentation
+% X = lhsdesign(n,p) returns an n-by-p matrix, X, containing a latin hypercube sample of n values on each of p variables.
+%For each column of X, the n values are randomly distributed with one from each interval (0,1/n), (1/n,2/n), ..., (1-1/n,1), and they are randomly permuted.
+%lhsdesign_modified provides a latin hypercube sample of n values of
+%each of p variables but unlike lhsdesign, the variables can range between
+%any minimum and maximum number specified by the user, where as lhsdesign
+%only provide data between 0 and 1 which might not be very helpful in many
+%practical problems where the range is not bound to 0 and 1
+%
+%Inputs: 
+%       n: number of radomly generated data points
+%       min_ranges_p: [1xp] or [px1] vector that contains p values that correspond to the minimum value of each variable
+%       max_ranges_p: [1xp] or [px1] vector that contains p values that correspond to the maximum value of each variable
+%Outputs
+%       X_scaled: [nxp] matrix of randomly generated variables within the
+%       min/max range that the user specified
+%       X_normalized: [nxp] matrix of randomly generated variables within the
+%       0/1 range 
+%
+%Example Usage: 
+%       [X_scaled,X_normalized]=lhsdesign_modified(100,[-50 100 ],[20  300]);
+%       figure
+%       subplot(2,1,1),plot(X_scaled(:,1),X_scaled(:,2),'*')
+%       title('Random Variables')
+%       xlabel('X1')
+%       ylabel('X2')
+%       grid on
+%       subplot(2,1,2),plot(X_normalized(:,1),X_normalized(:,2),'r*')
+%       title('Normalized Random Variables')
+%       xlabel('Normalized X1')
+%       ylabel('Normalized X2')
+%       grid on
+p=length(min_ranges_p);
+[M,N]=size(min_ranges_p);
+if M<N
+    min_ranges_p=min_ranges_p';
+end
+    
+[M,N]=size(max_ranges_p);
+if M<N
+    max_ranges_p=max_ranges_p';
+end
+slope=max_ranges_p-min_ranges_p;
+offset=min_ranges_p;
+SLOPE=ones(n,p);
+OFFSET=ones(n,p);
+for i=1:p
+    SLOPE(:,i)=ones(n,1).*slope(i);
+    OFFSET(:,i)=ones(n,1).*offset(i);
+end
+X_normalized = lhsdesign(n,p);
+X_scaled=SLOPE.*X_normalized+OFFSET;
+
 end
